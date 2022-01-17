081b49efa0ecabb8afa672446a097f93266c85af
aderama2711 <ade.ramadha@gmail.com>
2022-01-17 11:53:26 +0700

    std::cout

--- main.cpp
+++ main.cpp
@@ -89,7 +89,7 @@ int main(){
 		
 		double cpuused = getCurrentValue();
 		
-		cout << cpuused;
+		std::cout << cpuused;
 	
 	    auto now = std::chrono::system_clock::now();
 	    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
